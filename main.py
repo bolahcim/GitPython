075fbc7f0ad811daf0acc1331f417337cb3fcdc2
Michal Obrtanec <michal.obrtanec@braiins.cz>
2022-11-01 17:08:40 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import git
 
-
-repo = git.Repo.init('my_new_repo')
+repo_dir = 'my_new_repo'
+repo = git.Repo.init(repo_dir)
 
 # List all branches
 for branch in repo.branches:
     print(branch)
 
-
+# Will create empty main.py within your repo directory
+open(f'{repo_dir}/main.py', 'wb').close
 # Provide a list of the files to stage
 repo.index.add(['main.py'])
 # Provide a commit message
